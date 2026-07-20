c04ce56364d2e9925481f2a66bfc468aacabe27e
SayakGhosh006 <ghoshsayak909@gmail.com>
2026-07-20 20:39:38 +0530

    Time: 0 ms (100%), Space: 11.8 MB (72.5%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int func(vector<vector<int>>& obstacleGrid){
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        vector<int> prev(n,0);
+
+        for(int i=0;i<m;i++){
+            vector<int> temp(n,0);
+
+            for(int j=0;j<n;j++){
+
+                // If obstacle is present
+                if(obstacleGrid[i][j]==1){
+                    temp[j]=0;
+                    continue;
+                }
+
+                // Starting cell
+                if(i==0 && j==0){
+                    temp[j]=1;
+                    continue;
+                }
+
+                int up=0,left=0;
+
+                if(i>0)
+                    up=prev[j];
+
+                if(j>0)
+                    left=temp[j-1];
+
+                temp[j]=up+left;
+            }
+
+            prev=temp;
+        }
+
+        return prev[n-1];
+    }
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        return func(obstacleGrid);
+    }
+};
